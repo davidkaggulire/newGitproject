32f4a6a79b287f7de7e07e75a654896ccdcce038
davidkaggulire <dkaggulire@gmail.com>
2016-07-01 09:21:08 +0300

    made changes

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/xampp/');
-	exit;
+	$username = "username";
+	$password = "";
+	$db ="myapp";
+
+
+	$sql = new mysqli($username, $password, $db);
 ?>
-Something is wrong with the XAMPP installation :-(
+
