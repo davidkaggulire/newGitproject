dfa3bb9f84c3909e11a2462c87c81e1e30792080
davidkaggulire <dkaggulire@gmail.com>
2016-08-12 12:09:35 +0300

    added db_config.php

---
+++ db_config.php
@@ -0,0 +1,10 @@
+<?php
+    /**
+	 * init variabes
+	 * */
+     define('DB_SERVER', "localhost"); // db server
+	 define('DB_USER', "root"); // db user
+	 define('DB_PASSWORD', ""); // db password (mention your db password here)
+ 	 define('DB_DATABASE', "nrf"); // database name
+	 
+?>
